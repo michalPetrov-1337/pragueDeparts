9fd784a16be7acd0e885ac2512e86b4f84ea7a57
Michal Petrov <michal.petrov@post.cz>
2026-01-31 20:40:33 +0100

    Initial Commit

---
+++ PragueDepartures/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PragueDepartures
+//
+//  Created by Michal Petrov on 31.01.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
